0177b828e4ab4b8d9bcb9965d1a8f36afee5ec18
Hamda <h.sulaiman770@gmail.com>
2022-01-26 23:14:37 +0000

    project

---
+++ lib/pages/authentication/forgotpassword.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
+class forgotPasswordPage extends StatelessWidget {
+const forgotPasswordPage({Key? key}) : super(key: key);
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }}
